cfc745b42663b714e6d39ef42e22e56759e8580c
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-13 16:19:33 +0100

    React Count App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  }
+
+  const decrease = () => {
+    setCount(count - 1);
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
+
+  let color = '#fff';
+  if (count >= 1) {
+    color = 'green';
+  } else if (count < 0) {
+    color = 'red';
+  } else {
+    color = 'white';
+  }
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <section className="counter-sec --flex-center">
+      <div className="container counter --card --center-all">
+        <h1 className="--text-light">React Counter App</h1>
+        <p className="count --my2 --text-md --text-light --fw-bold" style={{ color: color }}>{count}</p>
+        <div className="buttons">
+          <button className="--btn --btn-danger" onClick={decrease}>- Substract</button>
+          <button className="--btn" onClick={reset}>! Reset</button>
+          <button className="--btn --btn-success" onClick={increase}>+ Add</button>
+        </div>
+      </div>
+    </section>
   );
 }
 
